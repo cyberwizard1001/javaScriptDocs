9243dcc9504cd38432dd676683d84863e2c16c74
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-04-29 14:19:26 +0530

    Completed functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,9 @@
+//Global variables
+//can be accessed by any fuction in file
+//NOTE: VARIABLE WITHOUT VAR KEYWORD IS GLOBAL
+var globalVar = "This variable is global";
+globslVar2 = 5;
+
 //Example
 function firstFunction(){
     console.log("Hello World");
@@ -6,4 +12,22 @@ function firstFunction(){
 //Calling a function
 firstFunction();
 
+//Passing parameters 
+function parameterFunction(a,b){
+    console.log(a-b);
+}
+
+parameterFunction(10,5);
+
+
+//NOTE: If local and global vars have same name, local var takes precedence
+
+//Function with return statement
+function numMinusSeven(a){
+    return a - 7;
+}
+
+console.log(numMinusSeven(10));
+
+//When a function doesn't return anything, the return value is considered "undefined".
 
